b894506e5fca7b8490199c39e827d296cccfc1a5
hrz123 <2403076194@qq.com>
2019-10-27 17:42:55 +0800

    Modify an error

--- watchlist/__init__.py
+++ watchlist/__init__.py
@@ -1,4 +1,3 @@
-from watchlist import views, errors, commands
 import os
 import sys
 
@@ -14,17 +13,15 @@ else:
     prefix = 'sqlite:////'
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.getenv(
-    'SECRET_KEY', 'dev')  # 等同于 app.secret_key = 'dev'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev')  # 等同于 app.secret_key = 'dev'
 app.config["SQLALCHEMY_DATABASE_URI"] = prefix + \
-    os.path.join(os.path.dirname(app.root_path),
-                 os.getenv('DATABASE_FILE', 'data.db'))
+    os.path.join(os.path.dirname(app.root_path), os.getenv('DATABASE_FILE', 'data.db'))
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
 
 
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)  # 实例化扩展类
-
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -32,12 +29,12 @@ def load_user(user_id):
     user = User.query.get(int(user_id))
     return user
 
-
 login_manager.login_view = 'login'
-
 
 @app.context_processor
 def inject_user():
     from watchlist.models import User
     user = User.query.first()
     return dict(user=user)
+
+from watchlist import views, errors, commands
